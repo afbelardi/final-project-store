b3b879223824bea90b3af2ae870b2e9fbace5a61
Alex <afbelardi@gmail.com>
2021-03-26 21:00:21 -0400

    products displayed flex, background color black added

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,27 +1,13 @@
 import React, { useState, useEffect } from 'react';
 
 export default function Products(props) {
-	const [products, setProducts] = useState([]);
-
-	useEffect(() => {
-		(async () => {
-			try {
-				const response = await fetch(`/api/photos`);
-				const data = await response.json();
-				await setProducts(data.data);
-			} catch (err) {
-				console.error(err);
-			}
-		})();
-	}, []);
-
 	return (
 		<main className="product-listings">
 			{Object.keys(props.data).length
 				? props.data.map(product => {
 						return (
 							<div id="product-card">
-								<img src={product.image} />
+								<img className="product-image" src={product.image} />
 								<h3>{product.title}</h3>
 							</div>
 						);
