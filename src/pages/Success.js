3e78af65bd66fc817ec7436177ccb5a31ffd1576
Alex <afbelardi@gmail.com>
2021-04-07 21:06:21 -0400

    page now redirects to success page after payment goes through successfully

--- src/pages/Success.js
+++ src/pages/Success.js
@@ -1,43 +1,12 @@
-// import styled from '@emotion/styled';
-// import Confetti from 'react-confetti';
-// import React, { useState, useEffect } from 'react';
-// import Layout from '../components/Layout';
+import React from 'react';
+import Header from '../components/Header';
 
-// import Row from '../components/prebuilt/Row';
-
-// const Container = styled.div`
-// 	width: 475px;
-// 	margin: 30px auto 0 auto;
-// 	text-align: center;
-// 	color: #fff;
-// `;
-
-// const Title = styled.div`
-// 	font-size: 58px;
-// `;
-
-// const Message = styled.div`
-// 	margin-top: 40px;
-// `;
-
-// export default function Success() {
-// 	const [width, setWidth] = useState(0);
-// 	const [height, setHeight] = useState(0);
-
-// 	useEffect(() => {
-// 		setTimeout(() => {
-// 			setWidth(window.innerWidth);
-// 			setHeight(window.innerHeight);
-// 		}, 100);
-// 	});
-
-// 	return (
-// 		<Layout title="Success!">
-// 			<Container>
-// 				<Confetti width={width} height={height} numberOfPieces={450} />
-// 				<Title>congrats!</Title>
-// 				<Message>Stripe has successfully processed your payment</Message>
-// 			</Container>
-// 		</Layout>
-// 	);
-// }
+export default function Success(props) {
+	return (
+		<div className="StorePage">
+			<div className="show-wrapper">
+				<Header />
+			</div>
+		</div>
+	);
+}
